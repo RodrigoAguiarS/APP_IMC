f04e6ba207ecaaea28749a139034c9e2339bdd77
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-09-22 18:00:36 -0300

    primeiro commit

---
+++ app/src/main/java/com/rodrigo/app_imc/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.rodrigo.app_imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.result_activity)
+    }
+}
